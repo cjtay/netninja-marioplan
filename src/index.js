4117947612b8c8e5af95e11be76e2ab4dcb34857
Tay CJ <cjtay@outlook.sg>
2020-07-06 20:09:43 +0800

    imac firebase data sync youtube lesson 19

--- src/index.js
+++ src/index.js
@@ -2,17 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import {
+    createFirestoreInstance,
+    getFirestore,
+    reduxFirestore,
+} from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import fbConfig from './config/firebaseconfig';
+import firebase from 'firebase/app';
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reduxFirestore(firebase, fbConfig)
+    )
+);
+
+const reactReduxFirebaseProps = {
+    firebase,
+    config: fbConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance,
+};
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <ReactReduxFirebaseProvider {...reactReduxFirebaseProps}>
+                <App />
+            </ReactReduxFirebaseProvider>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,8 +1,21 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         //make async calls
-
-        //dispatch actions
-        dispatch({ type: 'CREATE_PROJECT', project: project });
+        const firestore = getFirestore();
+        firestore
+            .collection('projects')
+            .add({
+                ...project,
+                authorFirstName: 'Net',
+                authorLastName: 'Ninja',
+                createdAt: new Date(),
+            })
+            .then(() => {
+                //dispatch actions
+                dispatch({ type: 'CREATE_PROJECT', project: project });
+            })
+            .catch((err) => {
+                dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+            });
     };
 };
